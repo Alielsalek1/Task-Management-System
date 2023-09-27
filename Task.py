3046f4f086eea86f05d21ff770dff2168ddc9f87
Mohamed-Amr7 <moh.amr777@gmail.com>
2023-09-28 01:48:23 +0300

    Implemented edit task method

--- Task.py
+++ Task.py
@@ -52,25 +52,6 @@ class Task:
     def status(self, status):
         self._status = status
 
-    def update(self, title=None, description=None, due_date=None, priority=None, status=None):
-        # Update task attributes if new values are provided
-        if title is not None:
-            self.title(title)
-        if description is not None:
-            self.description(description)
-        if due_date is not None:
-            self.due_date(due_date)
-        if priority is not None:
-            self.priority(priority)
-        if status is not None:
-            self.status(status)
-
-    def mark_completed(self):
-        self.status = True
-
-    def mark_incomplete(self):
-        self.status = False
-
     def get_details(self):
         # Get a formatted string containing task details
         status_str = "Completed" if self.status else "Incomplete"
@@ -121,6 +102,73 @@ class Task:
 
         # Add the created task to the user's task database
         task_manager.add_task_to_db(username, task)
+
+    @staticmethod
+    def truncate_text(text, max_length = 20):
+        return text[:max_length] + "..." if len(text) > max_length else text
+
+    @classmethod
+    def edit_task(cls, username):
+        task_manager = TaskManger.TaskManager(f"UserData/{username}/tasks.json")
+
+        # Retrieve all tasks for the user
+        all_tasks = task_manager.get_user_tasks(username)
+
+        # Check if there are tasks to edit
+        if not all_tasks:
+            return print("No Tasks to edit")
+
+        # List the titles of all tasks
+        task_manager.list_tasks_titles(all_tasks)
+
+        # Prompt the user to choose a task to edit
+        choice = int(InputValidators.check_number_in_range(1, len(all_tasks)))
+        choice -= 1
+
+        # Get the selected task object
+        task = Task(**all_tasks[choice])
+
+        print("Editing Task:")
+
+        # Prompt the user to edit the task title or 0 to skip
+        new_task_title = InputValidators.verify_argument_not_empty(
+            input(f"Enter a new task title ({cls.truncate_text(task.title)}) or 0 to skip: ").strip())
+        if new_task_title == "0":
+            new_task_title = task.title
+
+        # Prompt the user to edit the task description or press 0 to skip
+        new_task_description = InputValidators.verify_argument_not_empty(
+            input(f"Enter a new task description ({cls.truncate_text(task.description)}) or 0 to skip: ").strip())
+        if new_task_description == "0":
+            new_task_description = task.description
+
+        # Prompt the user to edit the due date or press 0 to skip
+        new_task_due_date = InputValidators.verify_due_date(
+            input(
+                f"Enter a new due date ({task.due_date}) or 0 to skip: ").strip())
+        new_task_due_date = task.due_date if not new_task_due_date else new_task_due_date
+
+        # Prompt the user to edit the task priority or press 0 to skip
+        new_task_priority = InputValidators.check_number_in_range(1, 10)
+        if new_task_priority == "0":
+            new_task_priority = task.priority
+
+
+        while True:
+            # Prompt the user to mark the task as completed or not
+            is_task_completed = input(f"Mark this task as completed (y/n)? ").strip().lower()
+            if is_task_completed in ("y", "n"):
+                is_task_completed = "Completed" if is_task_completed == "y" else "Incomplete"
+                break
+            else:
+                print("Please enter 'y' for yes or 'n' for no.")
+
+        new_task = Task(new_task_title, new_task_description, str(new_task_due_date), int(new_task_priority), is_task_completed)
+
+        # Save the updated task back to the user's task database
+        task_manager.update_task_in_db(username, task ,new_task)
+
+        print("Task updated successfully!")
 
     def __str__(self):
         # Get a string representation of the task
